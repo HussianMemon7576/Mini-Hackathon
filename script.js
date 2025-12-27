0fc84da2ad935a681e13994ecf70423ba132b324
HussianMemon7576 <memonhussain753@gmail.com>
2025-12-27 15:24:54 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,463 @@
+// for(let i=1; i<=1000;i++){
+//     console.log("HussainMemon");
+// }
+// let sum = 0;
+// for(let i=1; i<=5; i++){
+//     sum = sum +i;
+    
+// }
+// console.log("sum =",sum)
+// let i = 1;
+// while(i<=5){
+//     console.log("i+",i)
+//     i++
+// }
+
+// let sum = 0;
+// let i=1;
+
+// while(i<=5){
+   
+//   console.log("i=",i);
+//      sum = sum  +i;
+//       i++;
+      
+// }
+
+// let i = 1;
+
+// do{
+//     console.log("i=",i);
+//         i++
+    
+// } while(i<=5);
+
+// let string = "Hussain";
+// for(let i of string){
+//         console.log("i=",i);
+// }
+// for(let i=1;i<=100;i++){
+//     if(i %2!==0){
+//         console.log("i",i)
+//     } 
+
+// }
+
+// let i =1;
+
+// while(i<=100){
+//     console.log("i,",i)
+//     i++
+// }
+
+
+
+// let gameNum = 25;
+// let userNum = prompt("guess the game number");
+
+// while (userNum != gameNum){
+//     userNum = prompt("you entered wrong number");
+// }
+
+// console.log("congrulations, you entered the right number ");
+
+// let fullName = "Hussain";
+// console.log(fullName[0]);
+// console.log(fullName.length);
+
+// let real_name = "Hussain";
+// let friend =  "Hassan";
+// console.log(`His nama is ${real_name} and his friend nama is ${friend}` )
+
+// let f_name = hussain_Memon;
+// console.log("Hussain\nMemon");
+
+// let real_Name = "Hussain";
+// let newStr = real_Name.toLowerCase( );
+// console.log(newStr)
+
+// let name = "    hussain Memon  "
+// console.log(name.trim());
+
+
+
+// let num = "0123456";
+// console.log(num.slice(0,6))
+
+// let bhai = "hussain";
+// let full_name= "Memon";
+
+// // let res = bhai.concat(full_name);
+
+// let res = "Iam leraning from " + bhai + full_name;
+// console.log(res);
+
+// let lame = "Hassan"
+
+// console.log(lame.replace("an","ui"));
+
+
+// let lame = prompt("Enter your full Name");
+// let username = "@" + lame + lame.length;
+// console.log(username)
+
+// let num = prompt("enter your number");
+
+// for ( let i = 1; i<=10; i++ ){
+//     console.log(i * num);
+// }
+
+
+// let marks = prompt("enter your marks")
+// marks = Number(marks);
+
+// if(marks==100){
+//     console.log("congrulation, yo are gettion Top");
+// }
+//  else if (marks>=71 && marks<=89){
+//     console.log("Excellent! tou are gettion A Grade");
+
+// }
+//  else if (marks>=50 && marks<=69 ){
+//     console.log("you are getting B grade");
+
+// } 
+// else{
+//     console.log("Sorry! you are failed")
+// }
+
+// let marks =[96,82,33,45];
+// marks  [2] = [66];
+// console.log(marks);
+
+// let names = ["hassanAdnan" ,"Hussainmemon"];
+
+// for(i  =  0; i<names.length; i++ ){
+//     console.log(names[i]);
+// }
+
+// let cities = ["karachi","lahore","Multan"];
+// for(let city of cities){
+//     console.log(city.toUpperCase());
+// }
+// let numbers = [5, 10, 20, 15];
+// let sum = 0;
+
+// for(let i = 0; i < numbers.length; i++){
+//     sum = sum + numbers[i];
+// }
+
+// let avg = sum / numbers.length;
+
+// console.log(`average marks of class = ${avg}`);
+
+// let total = [300,960,340,100,397];
+// let i = 0;
+
+// for(let value of total){
+//     console.log(`value of index ${i} = ${value} `);
+
+//     let offer = value / 10;
+//     total[i] = total[i] - offer
+//     console.log(`value after offer = ${total[i]}`)
+//     i++;
+
+// }
+
+
+// let cities = ["Sukkur", "Karachi", "Lahore" ,]
+
+// // cities.push("Multan", "Delhi");
+// let deleteditem =cities.pop();
+// console.log("deleted", deleteditem)
+
+// let marks = [89,22,88,100];
+ 
+// console.log(marks.toString() );
+
+// let names = ["hussainMemon","hassanMemon"];
+// let father = ["Suleman","Adnan"];
+
+// let full_name = names.concat(father);
+// console.log(full_name);
+
+
+// let names = ["Karachi", "Hyderabad", "Lahore"];
+// let Delete = names.pop()
+// console.log("Deleted",Delete)
+
+// let marvelHeroes = ["Batman","Spiderman","daddy"]
+// // let dcHeroes = ["Logo","Hussain"];
+
+// // let heroes = marvelHeroes.concat(dcHeroes);
+// // console.log(heroes)
+//  let marvelHeroes = ["Batman","Spiderman","daddy"]
+//   let updatedvalue = marvelHeroes.shift();
+//  console.log(marvelHeroes);
+//   let marvelHeroes = ["Batman","Spiderman","daddy" , "Dr.strange", "Hulk"]
+//   console.log(marvelHeroes.slice(1,3));
+
+// let numbers = [1,2,3,4,5,6,7,];
+// numbers.splice(2,4, 101, 102);
+// console.log(numbers);
+
+// test 
+
+// let companies = ["bloomberg","Microsoft","Uber", "Google","Netflix","Amazon"];
+// companies.shift();
+// console.log(companies);
+// companies.splice(2,2,"Olla",)
+// console.log(companies);
+// companies.push("J.")
+// console.log(companies);
+
+// function full_Name(){
+//     console.log("my name is Hussain Memon");
+//     console.log("my father name is Muhammad Suleman");
+// }
+
+// full_Name();
+
+// function sum (a,b){
+//     console.log(a + b);
+// }
+
+// sum(22,43)
+//  let name = function sum(a,b){
+//     return a +b;
+// }
+//  let value = sum(3,5);
+//  console.log(value)
+
+// const multipllication = (a,b) =>{
+//     console.log (a *b);
+// }
+// //  console.log(multipllication(5,5));
+// multipllication(4,10)
+
+
+// function Vowels(str){
+//         let count = 0;
+//         for(const chrac of str){
+//             if(chrac === "a" || chrac === "e" || chrac === "i" || chrac ===  "o" || chrac === "u")
+// {
+//     count++;
+// }
+//         } 
+//             return count;
+//         }
+        
+    
+
+//         const countVo = (str) => {
+//   let count = 0;
+//         for(const chrac of str){
+//             if(chrac === "a" || chrac === "e" || chrac === "i" || chrac ===  "o" || chrac === "u")
+// {
+//     count++;
+// }
+//         } 
+//             return count;
+//         }
+
+//  let numbers = [2,4,6,8,10,20]
+
+//  numbers.forEach((numbers) => {
+//         console.log(numbers * numbers);
+//  })
+
+// function  names(username){
+//        return `${username} just logged in`;
+// }
+
+// console.log(names("Hussain"));
+
+
+
+
+// console.log(findLargest(50,100));
+// function findLargest(num1, num2) {
+//     if (num1 >= num2) {
+//         return num1 + " is Large";
+//     } else {
+//         return num2 + " is Large";
+//     }
+// }
+
+// console.log(findLargest(400, 100));
+
+// function Vowels(str){
+//         let count = 0;
+//         for(const chrac of str){
+//             if(chrac === "a" || chrac === "e" || chrac === "i" || chrac ===  "o" || chrac === "u")
+// {
+//     count++;
+// }
+//         } 
+//             return count;
+        // }
+
+
+        // let numbers = [2,6,9,20,12]
+
+        // numbers.forEach(function(num){
+        //     console.log(num*num);
+        // })
+
+
+        // console.log(typeof object   )
+
+        // let score = "33abc";
+
+        // let convertThenumber = Number(score);
+        // console.log( typeof convertThenumber);
+        // console.log(convertThenumber);
+
+        // let number = "";
+
+        // let numberTostring = Boolean(number);
+        // console.log(numberTostring)
+        // console.log( typeof numberTostring)
+
+        // // let str1 = "hello";
+        // // let str2 = " Hussain";
+        // // let str3 = str1 + str2;
+        // // console.log(str3)    
+
+        // let type = 1 + 2 + "2" + "2";
+        // console.log( type)
+
+
+
+        // console.log(+"")
+
+//         let name = "Hussain";
+//         let bigNumber = 4994294928442n;
+//         console.log(typeof bigNumber)
+//         let temp = null;
+//         console.log(typeof temp)
+// let id = Symbol("123");
+// console.log(typeof id)
+
+// let superHeroes = ["Ironman", "Spiderman", "Batman"]
+// let another = [0,1,2,3,4,5]
+// let equal = another.join()
+// console.log(equal)
+// console.log(another)
+// console.log( typeof equal)
+//  let b =superHeroes.unshift("Hussain") 
+// console.log(superHeroes) 
+
+// let slice  = [0,1,2,3,4,5]
+// console.log(slice)
+// let res = slice.slice(0,2)
+// console.log(res)
+
+
+// let dc_Heroes = ["batman", "Shaktian", "Doremon"]
+
+// let superHeroes = ["Ironman", "Spiderman", ]
+// const allHeroes = [...dc_Heroes,...superHeroes]
+// console.log(allHeroes)
+
+
+
+// let select = document.getElementById("toggle")
+// let black = document.getElementsByClassName("black")
+// let body = document.getElementById("body")
+
+// select.onchange = changeMode
+// function changeMode(){
+//      if(select.value == "dark"){
+//                 body.style.backgroundColor = "grey"
+//         } else {
+//                 body.style.backgroundColor = "white"
+//         }
+// }
+
+
+//    let value = document.getElementById("Num")
+//    let button = document.getElementById("Sum")
+//    let heading = document.getElementById("heading")
+
+//    function buttonclick(){
+//         if(Num.value > 18){
+//                 heading.innerText = "You are 18 plus"
+
+//         }
+//         else{
+//                 heading.innerText = "Watch Cartoon"
+//         }
+//    }
+   
+//    let value = document.getElementById("Num");
+//    let heading = document.getElementById("heading");
+
+//    function buttonclick(){
+//         let num = Number(value.value); // input ki value number me
+
+//         if(num > 18){
+//             heading.innerText = "You are 18 plus";
+//         }
+//         else{
+//             heading.innerText = "Watch Cartoon";
+//         }
+//    }
+
+// let symbol = symbol("key 1")
+// const sym1 = Symbol("key");
+const username = {
+        name : "Hussain",
+        full_name : "Memon",
+        isLoggedIn: true,
+        // [sym1]: "key",
+}
+// // username.full_name = "Ali";
+// // console.log(  username)
+
+// // // Object.freeze(username)
+// // console.log(typeof username[sym1])
+// // console.log(  username["name"])
+
+
+// username.greeting = function(){
+//         console.log(`hello,${this.full_name}` );
+// }
+// username.greeting()
+
+// let obj1 = {1: "a", 2 : "b"}
+// let obj2 = {3: "c", 4 : "d"}
+// const obj3 = Object.assign(obj1,obj2) 
+// console.log(obj3)
+
+// const username = {
+//         name : "Hussain",
+//         fullname : {
+//                 userfullname : {
+//                         email : "gmail@.com",
+//                         city : "Sukkur",
+//                 }
+
+
+//         }
+// }
+// console.log(username.fullname.userfullname)
+
+// console.log(Object.values(username))
+
+// const  {full_name : full} = username
+// console.log(full)
+
+function chalgya(){
+        let a = document.getElementById("image")
+        a.src = "https://www.w3schools.com/js/pic_bulbon.gif"
+         let c = document.getElementById("batao") 
+         c.innerText = "Bhai chal gye me"
+}
+function nhichala(){
+        let b = document.getElementById("image")
+        b.src = "https://www.w3schools.com/js/pic_bulboff.gif"
+            let c = document.getElementById("batao") 
+         c.innerText = "pese diye"
+}
+
